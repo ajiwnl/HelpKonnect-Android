0c0101f6c9c1cf56af82c15bdd22d99a8729b892
UnusualRuWei <moradafranzruie@gmail.com>
2024-10-02 18:22:00 +0800

    Resources to Music Player UI Activity

--- app/src/main/java/com/helpkonnect/mobileapp/ResourcesFragment.java
+++ app/src/main/java/com/helpkonnect/mobileapp/ResourcesFragment.java
@@ -1,20 +1,33 @@
 package com.helpkonnect.mobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 public class ResourcesFragment extends Fragment {
+
+    CardView musicPlayerContainer;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_resources, container, false);
+
+
+        musicPlayerContainer = rootView.findViewById(R.id.MusicPlayer);
+
+        musicPlayerContainer.setOnClickListener( v -> {
+            Intent toMusicPlayer = new Intent(requireContext(), MusicPlayerActivity.class);
+            startActivity(toMusicPlayer);
+        });
+
         return rootView;
     }
 }
