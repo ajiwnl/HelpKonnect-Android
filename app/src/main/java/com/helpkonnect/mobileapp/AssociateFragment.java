751ed4e405c7fbc224ce8206064fc1aa0c0a711c
UnusualRuWei <moradafranzruie@gmail.com>
2024-10-28 00:31:57 +0800

    AssociateFragment

---
+++ app/src/main/java/com/helpkonnect/mobileapp/AssociateFragment.java
@@ -0,0 +1,22 @@
+package com.helpkonnect.mobileapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AssociateFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_associate, container, false);
+
+        return rootView;
+    }
+}
