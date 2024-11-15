3994a777a5c4b81f2b61970cd53f73cd929b8a65
ajiwnl <alvinjohnaranetz@gmail.com>
2024-11-15 22:52:33 +0800

    removed shit commented code

--- app/src/main/java/com/helpkonnect/mobileapp/TrackerFragment.java
+++ app/src/main/java/com/helpkonnect/mobileapp/TrackerFragment.java
@@ -415,7 +415,7 @@ public class TrackerFragment extends Fragment {
             // Show the loading indicator
             showLoader(true, null);
 
-            String url = "http://192.168.1.9:5000/predict"; // Update with your API URL
+            String url = "http://192.168.1.9:5000/predict";
 
             JSONObject jsonBody = new JSONObject();
             try {
@@ -603,99 +603,5 @@ public class TrackerFragment extends Fragment {
             }
         }
     }
-
-   /* private void setDailyNotification() {
-        Log.d("TrackerFragment", "Setting up daily notification with OneSignal...");
-
-        // Check if the user hasn’t logged an entry today
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            String userId = user.getUid();
-
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.HOUR_OF_DAY, 0);
-            calendar.set(Calendar.MINUTE, 0);
-            Date startOfDay = calendar.getTime();
-
-            calendar.set(Calendar.HOUR_OF_DAY, 23);
-            calendar.set(Calendar.MINUTE, 59);
-            Date endOfDay = calendar.getTime();
-
-            Timestamp startTimestamp = new Timestamp(startOfDay);
-            Timestamp endTimestamp = new Timestamp(endOfDay);
-
-            db.collection("emotion_analysis")
-                    .whereEqualTo("journalUserId", userId)
-                    .whereGreaterThanOrEqualTo("dateCreated", startTimestamp)
-                    .whereLessThanOrEqualTo("dateCreated", endTimestamp)
-                    .get()
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful() && task.getResult().isEmpty()) {
-                            Log.d("TrackerFragment", "No entry for today. Sending reminder...");
-
-                            try {
-                                // Fetch the correct OneSignal player ID
-                                String oneSignalPlayerId = OneSignal.getDeviceState().getUserId();
-
-
-                                if (oneSignalPlayerId != null) {
-                                    // Create the notification content
-                                    JSONObject json = new JSONObject();
-                                    json.put("app_id", "3d064d90-5221-48da-aaa6-0702f8531c99");  // Replace with your OneSignal app ID
-
-                                    // Add title (headings) and content (contents)
-                                    JSONObject headings = new JSONObject();
-                                    headings.put("en", "Emotion Analysis Reminder");  // Set your desired title here
-                                    json.put("headings", headings);
-
-                                    JSONObject contents = new JSONObject();
-                                    contents.put("en", "It’s time to analyze your emotions today!");
-                                    json.put("contents", contents);
-                                    json.put("include_player_ids", new JSONArray().put(oneSignalPlayerId));
-
-
-                                    // Replace with your OneSignal REST API key
-                                    String apiKey = "os_v2_app_hude3ecsefenvkvga4bpquy4te4mxajyftqerken66en4mhhwrart2exdozrwrjhrf7sbivyriviti42pc32aev5ffkcwdsm26o63hy";  // Use your actual OneSignal API key here
-
-                                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                                            Request.Method.POST,
-                                            "https://onesignal.com/api/v1/notifications",
-                                            json,
-                                            new Response.Listener<JSONObject>() {
-                                                @Override
-                                                public void onResponse(JSONObject response) {
-                                                    Log.d("OneSignal", "Notification sent successfully: " + response.toString());
-                                                }
-                                            },
-                                            new Response.ErrorListener() {
-                                                @Override
-                                                public void onErrorResponse(VolleyError error) {
-                                                    Log.e("OneSignal", "Failed to send notification", error);
-                                                }
-                                            }) {
-                                        @Override
-                                        public Map<String, String> getHeaders() throws AuthFailureError {
-                                            Map<String, String> headers = new HashMap<>();
-                                            headers.put("Content-Type", "application/json");
-                                            headers.put("Authorization", "Basic " + apiKey);
-                                            return headers;
-                                        }
-                                    };
-
-                                    // Add the request to the queue
-                                    Volley.newRequestQueue(getContext()).add(jsonObjectRequest);
-                                } else {
-                                    Log.e("TrackerFragment", "Invalid OneSignal player ID.");
-                                }
-
-                            } catch (JSONException e) {
-                                Log.e("OneSignal", "Error creating notification request", e);
-                            }
-                        }
-                    });
-        }
-    }*/
-
 
 }
