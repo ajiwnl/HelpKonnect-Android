0edce12e8829953857f9052aeeb93b501dcbdd97
Soliken <iandanegomez.93@gmail.com>
2024-12-11 16:17:20 +0800

    Overhaul Changes of Booking Procedure/Process

--- app/src/main/java/com/helpkonnect/mobileapp/BookFacilityActivity.java
+++ app/src/main/java/com/helpkonnect/mobileapp/BookFacilityActivity.java
@@ -2,37 +2,27 @@ package com.helpkonnect.mobileapp;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -43,37 +33,30 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
 
-//Scratched Booking Process
-//Move to BookFacilityActivity
-
-public class ProfessionalDetailsActivity extends AppCompatActivity {
-
-    private ImageView professionalImage;
-    private TextView professionalName, associatedFacility;
+public class BookFacilityActivity extends AppCompatActivity {
+    private ImageView facilityImage;
+    private TextView facilityName, facilityDescription;
     private EditText startTimeEditText, sessionDurationEditText, availableDateEditText;
     private ImageButton backButton;
     private Button requestBookButton;
-    private String professionalId, userEmail, userUsername, userId, facility;
-    private float rate, amount, subtotal;
+    private String description, userId, facility;
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
-
-    private static final String BASE_URL = "https://helpkonnect.vercel.app/api/createCheckout";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_professional_details);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_book_facility);
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
 
-        professionalImage = findViewById(R.id.ProfessionalImage);
-        professionalName = findViewById(R.id.ProfessionalName);
-        associatedFacility = findViewById(R.id.AssociatedFacility);
+        facilityImage = findViewById(R.id.facilityImage);
+        facilityName = findViewById(R.id.facilityName);
+        facilityDescription = findViewById(R.id.facilityDescription);
         availableDateEditText = findViewById(R.id.AvailableDateEditText);
         startTimeEditText = findViewById(R.id.StartTimeEditText);
-        backButton = findViewById(R.id.ProfessionalBackButton);
+        backButton = findViewById(R.id.BookingBackButton);
         requestBookButton = findViewById(R.id.RequestBookButton);
         sessionDurationEditText = findViewById(R.id.SessionDurationEditText);
 
@@ -81,16 +64,14 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
 
         if (getIntent().getExtras() != null) {
             String imageUrl = getIntent().getStringExtra("imageUrl");
-            String name = getIntent().getStringExtra("name");
-            facility = getIntent().getStringExtra("facilityName");
-            professionalId = getIntent().getStringExtra("userId");
-            rate = getIntent().getFloatExtra("rate", 0.0f);
+            facility = getIntent().getStringExtra("name");
+            description = getIntent().getStringExtra("description");
             Glide.with(this)
-                    .load(imageUrl) 
-                    .into(professionalImage); 
+                    .load(imageUrl)
+                    .into(facilityImage);
 
-            professionalName.setText(name);
-            associatedFacility.setText("Associated with " + facility);
+            facilityName.setText(facility);
+            facilityDescription.setText(description);
         }
 
         availableDateEditText.setOnClickListener(v -> showDatePickerDialog());
@@ -99,7 +80,6 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
             userId = currentUser.getUid();
-            fetchUserCredentials(userId);
         }
 
         requestBookButton.setOnClickListener(v -> {
@@ -124,10 +104,7 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
                     return;
                 }
 
-                float amount = Float.parseFloat(amountText);
-                subtotal = amount * rate;
-
-                createCheckoutSession(subtotal);
+                saveBookingDetails(userId, sessionDurationEditText.getText().toString());
 
             } catch (ParseException e) {
                 e.printStackTrace();
@@ -137,22 +114,6 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
                 Toast.makeText(this, "Please enter a valid amount", Toast.LENGTH_SHORT).show();
             }
         });
-
-    }
-    private void fetchUserCredentials(String userId) {
-        db.collection("credentials")
-                .whereEqualTo("userId", userId)
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            userEmail = document.getString("email");
-                            userUsername = document.getString("username");
-                        }
-                    } else {
-                        Log.e("UserCredentials", "Error getting documents: ", task.getException());
-                    }
-                });
     }
 
     private void showDatePickerDialog() {
@@ -169,7 +130,7 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
         datePickerDialog.show();
     }
 
-    private void saveBookingDetails(String userId, String professionalId, float amount, String sessionDuration) {
+    private void saveBookingDetails(String userId, String sessionDuration) {
         String selectedDate = availableDateEditText.getText().toString();
         String selectedTime = startTimeEditText.getText().toString();
 
@@ -178,11 +139,12 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
         Map<String, Object> booking = new HashMap<>();
         booking.put("bookingId", UUID.randomUUID().toString());
         booking.put("userId", userId);
-        booking.put("professionalId", professionalId);
-        booking.put("amount", amount);
+        booking.put("professionalId", "");
+        booking.put("amount", 0);
         booking.put("sessionDuration", sessionDuration);
         booking.put("bookingDate", bookingDateTime);
         booking.put("createdAt", FieldValue.serverTimestamp());
+        booking.put("status", false);
         booking.put("facilityName", facility);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -191,71 +153,12 @@ public class ProfessionalDetailsActivity extends AppCompatActivity {
                 .add(booking)
                 .addOnSuccessListener(documentReference -> {
                     Log.d("Booking", "Booking saved with ID: " + documentReference.getId());
+                    Toast.makeText(this, "Successfully Booked! Please Wait For The Facility To Allocate Your Booking Schedule", Toast.LENGTH_SHORT).show();
                 })
                 .addOnFailureListener(e -> {
                     Log.e("Booking", "Error saving booking: ", e);
                 });
     }
-
-    private void createCheckoutSession(float subtotal) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-        JSONObject requestBody = new JSONObject();
-        try {
-            requestBody.put("title", facility);
-            requestBody.put("amount", subtotal);
-            requestBody.put("username", userUsername);
-            requestBody.put("email", userEmail);
-            requestBody.put("phone", "");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.POST,
-                BASE_URL,
-                requestBody,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            String checkoutUrl = response.getJSONObject("data").getJSONObject("attributes").getString("checkout_url");
-                            Log.d("CheckoutSession", "Checkout URL: " + checkoutUrl);
-
-                            // Open the third-party URL in a web browser
-                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(checkoutUrl));
-                            startActivity(intent);
-
-                            // Save booking details
-                            saveBookingDetails(userId, professionalId, subtotal, sessionDurationEditText.getText().toString());
-
-                            Toast.makeText(ProfessionalDetailsActivity.this, "Creating your facility booking! Redirecting...", Toast.LENGTH_SHORT).show();
-
-                            // Navigate back to the FacilitiesFragment after some delay
-                            new Handler().postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    // Go back to FacilitiesFragment
-                                    onBackPressed();
-                                }
-                            }, 3000);  // Delay 3 seconds before navigating back
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("CheckoutSession", "Request failed: " + error.getMessage());
-                    }
-                }
-        );
-
-        queue.add(jsonObjectRequest);
-    }
-
 
     private void showTimePickerDialog() {
         final Calendar calendar = Calendar.getInstance();
